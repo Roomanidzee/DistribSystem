c43494f2c578239e0b45ab0800063a961de2f58a
Daniil <doriboty@gmail.com>
2017-07-08 11:26:14 +0300

    Create models 'Professor' and 'ScientificDirector'

--- distrib_system/userroles/models.py
+++ distrib_system/userroles/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .fields import AutoOneToOneField    
-    
-class Cooperator(models.Model):
-    
-    #роль 'Сотрудник'
-    surname = models.CharField(max_length = 100, verbose_name = 'Фамилия')
+from .fields import AutoOneToOneField
+
+class AbstractPerson(models.Model):
+
+	surname = models.CharField(max_length = 100, verbose_name = 'Фамилия')
     name = models.CharField(max_length = 100, verbose_name = 'Имя')
     patronymic = models.CharField(max_length = 100, verbose_name = 'Отчество')
     email = models.EmailField(max_length = 50, verbose_name = 'Почта')
@@ -17,6 +16,22 @@ class Cooperator(models.Model):
         return "{0} {1} {2}, login: {3}, password : {4}, work : {5}".format(
             self.surname, self.name, self.patronymic, self.email, self.password, self.work)
     
+class Cooperator(AbstractPerson):
+    
+    #роль 'Сотрудник'
+   
+class Professor(AbstractPerson):
+	
+	#роль 'Преподаватель'
+	education_course = CharField(max_length = 100, verbose_name = 'Предмет')
+	is_lecturer = BooleanField(verbose_name = 'Лектор')
+	is_practician = BooleanField(verbose_name = 'Преподаватель практики')
+
+class ScientificDirector(AbstractPerson):
+	
+	#роль 'Научный руководитель'
+	education_course = CharField(max_length = 100, verbose_name = 'Предмет')
+	
 class Profile(models.Model):
     
     user = AutoOneToOneField(User, related_name = 'profile', verbose_name=('User'), primary_key = True)
