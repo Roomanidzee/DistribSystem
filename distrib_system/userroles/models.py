3b31c292c69ee5b6ddc299aecce941c71c970230
Andrey Romanov <steampart@gmail.com>
2017-07-08 11:03:13 +0300

    Added str method

--- distrib_system/userroles/models.py
+++ distrib_system/userroles/models.py
@@ -8,9 +8,14 @@ class Cooperator(models.Model):
     surname = models.CharField(max_length = 100, verbose_name = 'Фамилия')
     name = models.CharField(max_length = 100, verbose_name = 'Имя')
     patronymic = models.CharField(max_length = 100, verbose_name = 'Отчество')
-    email = models.CharField(max_length = 50, verbose_name = 'Почта')
+    email = models.EmailField(max_length = 50, verbose_name = 'Почта')
     password = models.CharField(max_length = 16, verbose_name = 'Пароль')
     work = models.CharField(max_length = 50, verbose_name = 'Специализация')
+    
+    def __str__(self):
+        
+        return "{0} {1} {2}, login: {3}, password : {4}, work : {5}".format(
+            self.surname, self.name, self.patronymic, self.email, self.password, self.work)
     
 class Profile(models.Model):
     
