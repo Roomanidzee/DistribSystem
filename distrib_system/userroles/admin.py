557002364a57a5b18f5ca3a9e9a017e4a1c1a9a4
Oleg <numeralhuman@gmail.com>
2017-07-09 12:39:40 +0300

    Admin options + refactoring models (simplified)

--- distrib_system/userroles/admin.py
+++ distrib_system/userroles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cooperator, Student, Professor, ScientificDirector, Profile
 
 # Register your models here.
+admin.site.register(Cooperator)
+admin.site.register(Student)
+admin.site.register(Professor)
+admin.site.register(ScientificDirector)
+admin.site.register(Profile)
--- distrib_system/userroles/models.py
+++ distrib_system/userroles/models.py
@@ -1,60 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .fields import AutoOneToOneField   
- 
-class AbstractPerson(models.Model):
-    surname = models.CharField(max_length = 100, verbose_name = 'Фамилия')
-    name = models.CharField(max_length = 100, verbose_name = 'Имя')
-    patronymic = models.CharField(max_length = 100, verbose_name = 'Отчество')
     
-    
-class Cooperator(AbstractPerson):
+class Cooperator(models.Model):
     #роль 'Сотрудник'
-    work = models.CharField(max_length = 50, verbose_name = 'Специализация')
+    work = models.CharField(max_length = 100, verbose_name = 'Специализация')
     user = AutoOneToOneField(User, related_name = 'cooperator', verbose_name=('User'), primary_key = True)
 
     def __str__(self):
-        return "{0} {1} {2}, work: {3}".format(
-            self.surname, self.name, self.patronymic, self.work)
+        return "{0} - {1}".format(str(self.user), self.work)
         
-        
-class Student(AbstractPerson):
+class Student(models.Model):
     #роль 'Студент'
     group = models.CharField(max_length = 10, verbose_name = 'Группа студента')
-    course = models.CharField(max_length = 10, verbose_name = 'Курс студента')
     user = AutoOneToOneField(User, related_name = 'student', verbose_name=('User'), primary_key = True)
     
     def __str__(self):
-        return "{0} {1} {2}, group: {3}, course: {4}".format(
-            self.surname, self.name, self.patronymic, self.group, self.course)
+        return "{0} - {1}".format(str(self.user), self.group)
         
-
-class Professor(AbstractPerson):
+class Professor(models.Model):
     #роль 'Преподаватель'
     education_course = models.CharField(max_length = 100, verbose_name = 'Предмет')
     user = AutoOneToOneField(User, related_name = 'professor', verbose_name=('User'), primary_key = True)
     
     def __str__(self):
-        return "{0} {1} {2}, education_course: {3}".format(
-			self.surname, self.name, self.patronymic, self.education_course)
+        return "{0} - {1}".format(str(self.user), self.education_course)
 
-class ScientificDirector(AbstractPerson):
+class ScientificDirector(models.Model):
     #роль 'Научный руководитель'
     education_course = models.CharField(max_length = 100, verbose_name = 'Предмет')
     user = AutoOneToOneField(User, related_name = 'scientific_director', verbose_name=('User'), primary_key = True)
-
-    def __str__(self):
-        return "{0} {1} {2}, education_course: {3}".format(
-            self.surname, self.name, self.patronymic, self.education_course)
-             
-class Profile(models.Model):
-    user = AutoOneToOneField(User, related_name = 'profile', verbose_name=('User'), primary_key = True)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-
-    def __str__(self):
-        
-        return "Bio : {0}\n, location: {1}, birth date: {2}".format(
-            self.bio, self.location, self.birth_date)
     
+    def __str__(self):
+        return "{0} - {1}".format(str(self.user), self.education_course)
