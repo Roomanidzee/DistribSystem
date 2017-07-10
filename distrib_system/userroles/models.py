db9a1996bcbde0e544f6e0858974ac8804957d1b
Andrey Romanov <steampart@gmail.com>
2017-07-10 15:39:03 +0300

    Added db

--- distrib_system/userroles/models.py
+++ distrib_system/userroles/models.py
@@ -7,6 +7,6 @@ class UserProfile(models.Model):
     patronymic = models.CharField(max_length = 50, verbose_name = 'Отчество')
 
     def user_post_save(self, sender, instance, **kwargs):
-        ( profile, new ) = UserProfile.objects.get_or_create(user=instance)
+       ( profile, new ) = UserProfile.objects.get_or_create(user=instance)
  
 models.signals.post_save.connect(UserProfile.user_post_save, sender=User)     
--- distrib_system/userroles/urls.py
+++ distrib_system/userroles/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from . import views
+from .views import register
 
 urlpatterns = [
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', 'django.contrib.auth.views.login', { "template_name": "accounts/login.html" }),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'), 
+    url(r'^register/$', register, name='register'),
+    #url(r'^login/$', 'django.contrib.auth.views.login', { "template_name": "login.html" }),
+    #url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'), 
 ]
