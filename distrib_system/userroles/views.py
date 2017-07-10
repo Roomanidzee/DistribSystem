f658d5f089762f5efce3558ca279dd81ca3f8072
Daniil <doriboty@gmail.com>
2017-07-10 16:59:45 +0300

    Registration fix

--- distrib_system/userroles/views.py
+++ distrib_system/userroles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 
@@ -11,15 +11,13 @@ from .models import UserProfile
 # Create your views here.
 
 def register(request):
-    
-    user = User()
-    user_profile = UserProfile()
-        
-    user_form = UserForm(request.POST, instance = user)
-    user_profile_form = UserProfileForm(request.POST, instance = user_profile)
+    user_form = UserForm(request.POST or None)
+    user_profile_form = UserProfileForm(request.POST or None)
         
     if user_form.is_valid() and user_profile_form.is_valid():
-            
+        
+        user = user_form.save(commit=False)
+        user_profile = user_profile_form.save(commit=False)
         user_data = user_form.cleaned_data
         user.username = user_data['username']
         user.last_name = user_data['last_name']
@@ -30,14 +28,13 @@ def register(request):
             
         user_profile = user.get_profile()
         user_profile_data = user_profile_form.cleaned_data
-        user_profile.type = user_profile_data['type']
         user_profile.patronymic = user_profile_data['patronymic']
         user_profile.save()
             
-        user = authenticate(username = user_data['username'], password = user_data['pass1'])
-        login(request, user)
-        return HttpResponse("profile/")
-        
+        user = authenticate(username = user.username, password = user.password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("<h1>Welcome</h1>")
     context = {
         "form": user_form,
         "profile_form": user_profile_form,
