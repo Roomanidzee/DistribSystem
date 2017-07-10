04cd5d022e1c7f2640caf4880aff44b6ade91e75
Daniil <doriboty@gmail.com>
2017-07-10 16:15:09 +0300

    Some fixes

--- distrib_system/userroles/forms.py
+++ distrib_system/userroles/forms.py
@@ -1,20 +1,25 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 class UserForm(forms.ModelForm):
     
     username = forms.CharField(max_length = 50, label = "Имя пользователя")
-    lastname = forms.CharField(max_length = 50, required = True, label = "Фамилия")
-    firstname = forms.CharField(max_length = 50, required = True, label = "Имя")    
+    last_name = forms.CharField(max_length = 50, required = True, label = "Фамилия")
+    first_name = forms.CharField(max_length = 50, required = True, label = "Имя")    
     patronymic = forms.CharField(max_length = 50, required = True, label = "Отчество") 
     
     email = forms.EmailField(widget = forms.EmailInput, label = "Электронная почта")
-    pass1 = forms.CharField(widget = forms.PasswordInput, label = "Пароль")
+    password = forms.CharField(widget = forms.PasswordInput, label = "Пароль")
     pass2 = forms.CharField(widget = forms.PasswordInput, label = "Повторите пароль")
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'first_name', 'last_name']
     
     def clean_pass2(self):
         
-        if(self.cleaned_data["pass2"] != self.cleaned_data.get("pass1", "")):
+        if(self.cleaned_data["pass2"] != self.cleaned_data.get("password", "")):
             
             raise forms.ValidationError("Пароли не свопадают")  
         
@@ -22,4 +27,9 @@ class UserForm(forms.ModelForm):
     
 class UserProfileForm(forms.ModelForm):
     
-    type = forms.ChoiceField(widget = forms.Select, label = "Должность")        
+    type = forms.ChoiceField(widget = forms.Select, label = "Должность")
+    patronymic = forms.CharField(max_length = 50, required = True, label = "Отчество")         
+    
+    class Meta:
+        model = UserProfile
+        fields = ['patronymic']
