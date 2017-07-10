dab982218751c9037d2ddb255802b70ccde76633
Andrey Romanov <steampart@gmail.com>
2017-07-10 16:17:50 +0300

    Fixed UserProfile

--- distrib_system/userroles/models.py
+++ distrib_system/userroles/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User, Group
     
 class UserProfile(models.Model):
     
     user = models.OneToOneField(User)
+    
+def create_profile(sender, **kwargs):
+    
+    user = kwargs["instance"]
+    if kwargs["created"]:
+        user_profile = UserProfile(user=user)
+        user_profile.save()
+post_save.connect(create_profile, sender=User)    
