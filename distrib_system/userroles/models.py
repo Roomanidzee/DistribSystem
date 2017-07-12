3ed287b4cf506be686f21baef662273d18639e76
Daniil <doriboty@gmail.com>
2017-07-12 13:45:01 +0300

    Some updates and fixes
    Updated models, utils, forms, views. Fixed urls.

--- distrib_system/userroles/models.py
+++ distrib_system/userroles/models.py
@@ -3,21 +3,26 @@ from django.contrib.auth.models import User
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, primary_key=True)
     group = models.CharField(max_length=10, blank=True)
     course = models.CharField(max_length=20, blank=True)
-
+    def __str__(self):
+        return self.user.__str__()
 
 class Cooperator(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, primary_key=True)
     work = models.CharField(max_length=100, blank=True)
-
+    def __str__(self):
+        return self.user.__str__()
 
 class Professor(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, primary_key=True)
     education_course = models.CharField(max_length=100, blank=True)
-
+    def __str__(self):
+        return self.user.__str__()
 
 class ScientificDirector(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, primary_key=True)
     education_course = models.CharField(max_length=100, blank=True)
+    def __str__(self):
+        return self.user.__str__()
--- distrib_system/userroles/urls.py
+++ distrib_system/userroles/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.contrib.auth import views as djangoviews
 from . import views
 
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', 'django.contrib.auth.views.login', { "template_name": "accounts/login.html" }),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
+    url(r'^login/$', djangoviews.login, { "template_name": "login.html" }),
+    url(r'^logout/$', djangoviews.logout_then_login),
     url(r'^my_profile/(?P<user_id>[0-9]+)/$', views.my_profile, name='profile'),
     url(r'^my_profile/(?P<user_id>[0-9]+)/editprof/$', views.edit_profile, name='edit_profile'),
     url(r'^my_profile/(?P<user_id>[0-9]+)/editpass/$', views.edit_password, name='edit_password'),
