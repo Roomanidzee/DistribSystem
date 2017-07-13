0b021dcc4f115387688404368831e20daad8702c
Oleg <numeralhuman@gmail.com>
2017-07-13 14:19:05 +0300

    Added functionality from choose_distrib to administration

--- distrib_system/choose_distrib/admin.py
+++ distrib_system/choose_distrib/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Container, Laboratory, Course, Practice, ScienceHead, Request, StudentToLabStorage
 # Register your models here.
+
+admin.site.register(Container)
+admin.site.register(Laboratory)
+admin.site.register(Course)
+admin.site.register(Practice)
+admin.site.register(ScienceHead)
+admin.site.register(Request)
+admin.site.register(StudentToLabStorage)
