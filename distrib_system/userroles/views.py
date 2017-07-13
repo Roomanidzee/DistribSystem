61a9665b340da0a035dfa2b4d85a96e089bb9b88
D0ct0rZl0 <doctorzlo1177@gmail.com>
2017-07-13 20:04:02 +0300

    Wrong login message

--- distrib_system/userroles/views.py
+++ distrib_system/userroles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
@@ -12,25 +13,24 @@ from .utils import get_entity_from_db, initialize_user
 
 
 def register(request):
-    
     if request.method == "POST":
         user = User(username=request.POST['username'])
         user.set_password(request.POST['password'])
-        user.first_name=request.POST['first_name']
-        user.last_name=request.POST['last_name']
-        user.email=request.POST['email']
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
         user.save()
         positions = request.POST.getlist('position')
         initialize_user(user, positions)
         user = authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             login(request, user)
+            messages.add_message(request, messages.INFO, 'Incorrect')
             return HttpResponseRedirect('/accounts/my_profile/' + str(user.id))
     return render(request, 'register.html')
 
 
 def new_login(request):
-    
     if request.POST:
         username = request.POST.get('login')
         password = request.POST.get('password')
@@ -42,19 +42,21 @@ def new_login(request):
                 return HttpResponseRedirect('/accounts/my_profile/' + str(user.id))
             else:
                 raise Http404
-    
-        else:            
+        else:
+            messages.add_message(request, messages.INFO, 'Неправильный логин или пароль')
+            mess = messages.get_messages(request)
             return render(request, 'login.html')
     else:
         return render(request, 'login.html')
-        
+
+
 def new_logout(request):
     
     logout(request)
     return HttpResponseRedirect('/accounts/login/')
                 
 
-@login_required(login_url = '/accounts/login')
+@login_required(login_url='/accounts/login')
 def my_profile(request, user_id):
 
     """Профиль текущего пользователя"""
@@ -70,11 +72,11 @@ def my_profile(request, user_id):
     return render(request, "accounts/my_profile.html", context)
 
 
-@login_required(login_url = '/accounts/login')
+@login_required(login_url='/accounts/login')
 def edit_profile(request, user_id):    
     pass
 
 
-@login_required(login_url = '/accounts/login')
+@login_required(login_url='/accounts/login')
 def edit_password(request, user_id):    
     pass
