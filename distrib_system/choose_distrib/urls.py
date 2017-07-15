e0c41929676bec028e138e952d3f17969efc27b7
D0ct0rZl0 <doctorzlo1177@gmail.com>
2017-07-15 15:09:26 +0300

    tabulations

--- distrib_system/choose_distrib/urls.py
+++ distrib_system/choose_distrib/urls.py
@@ -8,13 +8,13 @@ urlpatterns = [
                                                         url(r'(?P<request_type>[A-z]+)/', views.student_form, name='student'),
                                                         #url(r'^(?P<request_type>[A-Z])/make_request/(?P<destination_id>)', views.student_make_request, name='student_make_request'),
                                                         ])),
-                                                    #url(r'^professor/', include([
-                                                    #    url(r'^(?P<request_type>[A-Z])/', views.professor_form, name='professor'),
-                                                    #    url(r'^(?P<request_type>[A-Z])/(?P<user_id2>[0-9]+)/(?P<request_status>[1-2])/', views.professor_request_change_status, name='professor_request_change_status')])),
-                                                    #url(r'^sc_dir/', include([
-                                                    #    url(r'^(?P<request_type>[A-Z]+)/', views.sci_dir_form, name='sc_dir_sci_dir'),
-                                                    #    url(r'^(?P<request_type>[A-Z]+)/(?P<user_id2>[0-9]+)/(?P<request_status>[1-2])/', views.sc_dir_request_change_status, name='sc_dir_request_change_status')])),
-                                                    #url(r'^cooperator/', include([
-                                                    #    url(r'^(?P<request_type>[A-Z]+)/', views.coop_requests),])),
-                                                    ])),
+                                                        #url(r'^professor/', include([
+                                                        #    url(r'^(?P<request_type>[A-Z])/', views.professor_form, name='professor'),
+                                                        #    url(r'^(?P<request_type>[A-Z])/(?P<user_id2>[0-9]+)/(?P<request_status>[1-2])/', views.professor_request_change_status, name='professor_request_change_status')])),
+                                                        #url(r'^sc_dir/', include([
+                                                        #    url(r'^(?P<request_type>[A-Z]+)/', views.sci_dir_form, name='sc_dir_sci_dir'),
+                                                        #    url(r'^(?P<request_type>[A-Z]+)/(?P<user_id2>[0-9]+)/(?P<request_status>[1-2])/', views.sc_dir_request_change_status, name='sc_dir_request_change_status')])),
+                                                        #url(r'^cooperator/', include([
+                                                        #    url(r'^(?P<request_type>[A-Z]+)/', views.coop_requests),])),
+                                                        ])),
 ]
